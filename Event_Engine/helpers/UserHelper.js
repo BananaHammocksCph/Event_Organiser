c04f492faaf0be7b6d829503f623f9ddf354a19a
sean <seanaltoft@gmail.com>
2019-12-11 15:20:27 +0100

    Add user connection DTO

---
+++ Event_Engine/helpers/UserHelper.js
@@ -0,0 +1,9 @@
+// DTO for transfering a user connection to the VPN_VALIDATOR_SERVICE
+function userConnectionDTO(user, ip, coords) {
+  return {
+  id: user._id,
+  ip: ip,
+  coordinates: coords,
+  date: Date.now()
+  };
+}
